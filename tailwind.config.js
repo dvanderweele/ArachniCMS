de49704b43dcb7b3245a4302617bb8c3eff2aae7
Dave VanderWeele <weele.me@gmail.com>
2019-06-07 15:25:30 -0500

    Started tailwind config for theme switcher

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    extend: {}
+  },
+  colors: {
+    background: {
+      primary: 'var(--bg-background-primary)',
+      secondary: 'var(--bg-background-secondary)',
+      tertiary: 'var(--bg-background-tertiary)',
+
+      form: 'var(--bg-background-form)',
+    },
+    copy: {
+      primary: 'var(--text-copy-primary)',
+      secondary: 'var(--text-copy-hover)',
+    },
+},
+  variants: {},
+  plugins: []
+}
