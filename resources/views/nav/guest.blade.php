9a9821d06fe943cf760ecc50b32d440278428164
Dave VanderWeele <weele.me@gmail.com>
2019-06-08 13:53:41 -0500

    Added client logos to nav + small mods

--- resources/views/nav/guest.blade.php
+++ resources/views/nav/guest.blade.php
@@ -1,26 +1,32 @@
-<nav class="min-w-full py-4 m-0 flex flex-row items-center flex-wrap justify-around font-sans">
-  <div class="flex flex-row items-center py-2">
-    <img class="h-10 mr-8" src="{{ asset('storage/icons/about.svg') }}" alt="An icon portraying a generic online profile.">
-    <a href="#">
-      About
-    </a>
+<nav class="min-w-full py-4 m-0 flex flex-row items-center flex-wrap justify-around font-sans px-6">
+  <div class="flex flex-row items-center">
+    <img class="h-16" src="{{ asset('storage/icons/skull-static.PNG') }}" alt="A skull with a gear around the outside.">
+    <a href="#"><img class="h-16" src="{{ asset('storage/icons/rsb.PNG') }}" alt="The logo for Ruthies Space Bus."></a>
   </div>
-  <div class="flex flex-row items-center py-2">
-    <img class="h-12 mr-8" src="{{ asset('storage/icons/music.svg') }}" alt="An icon portraying a small people playing on giant musical notes.">
-    <a href="#">
-      Music
-    </a>
-  </div>
-  <div class="flex flex-row items-center py-2">
-    <img class="h-12 mr-8" src="{{ asset('storage/icons/blog_post.svg') }}" alt="An icon portraying a blog."> 
-    <a href="#">
-      Blog
-    </a>
-  </div>
-  <div class="flex flex-row items-center py-2">
-    <img class="h-12 mr-8" src="{{ asset('storage/icons/contact.svg') }}" alt="An icon portraying various ways of contacting a business, such as email and phone.">
-    <a href="#">
-      Contact
-    </a>
+  <div class="flex flex-row items-center justify-between flex-wrap">
+    <div class="flex flex-row items-center py-2 mr-8">
+      <img class="h-10 mr-4" src="{{ asset('storage/icons/about.svg') }}" alt="An icon portraying a generic online profile.">
+      <a href="#">
+        About
+      </a>
+    </div>
+    <div class="flex flex-row items-center py-2 mr-8">
+      <img class="h-12 mr-4" src="{{ asset('storage/icons/music.svg') }}" alt="An icon portraying a small people playing on giant musical notes.">
+      <a href="#">
+        Music
+      </a>
+    </div>
+    <div class="flex flex-row items-center py-2 mr-8">
+      <img class="h-12 mr-4" src="{{ asset('storage/icons/blog_post.svg') }}" alt="An icon portraying a blog."> 
+      <a href="#">
+        Blog
+      </a>
+    </div>
+    <div class="flex flex-row items-center py-2 mr-8">
+      <img class="h-12 mr-4" src="{{ asset('storage/icons/contact.svg') }}" alt="An icon portraying various ways of contacting a business, such as email and phone.">
+      <a href="#">
+        Contact
+      </a>
+    </div>
   </div>
 </nav>
