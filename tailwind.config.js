2af526d3f5683d047768b8f2646ec6700b8a2424
Dave VanderWeele <weele.me@gmail.com>
2019-06-17 12:02:28 -0500

    Pulled in 3rd party package to tailwindify pagination

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,23 @@ module.exports = {
         secondary: 'var(--text-copy-secondary)',
       },
     },
+    pagination: {
+      link: 'bg-background-primary px-3 py-1 border-r border-t border-b text-copy-primary no-underline',
+      linkActive: 'bg-background-secondary font-bold',
+      linkSecond: 'rounded-l border-l',
+      linkBeforeLast: 'rounded-r',
+      linkFirst: {
+          '@apply mr-3 pl-5 border': {},
+          'border-top-left-radius': '999px',
+      },
+      linkLast: {
+          '@apply ml-3 pr-5 border': {},
+          'border-top-right-radius': '999px',
+      },
+    },
   },
   variants: {},
-  plugins: []
+  plugins: [
+    require('tailwindcss-plugins/pagination'),
+  ]
 }
