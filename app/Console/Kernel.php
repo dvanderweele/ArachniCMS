c327752173937e59ddf24f1b25d0e5fc5f1d725f
Dave VanderWeele <weele.me@gmail.com>
2019-07-19 13:14:36 -0500

    Modified console kernel schedule so sitemapgeneratorjob runs daily at midnight

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,8 @@
 
 namespace App\Console;
 
+use App\Jobs\SitemapGeneratorJob;
+
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -26,6 +28,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->job(new SitemapGeneratorJob)->daily();
     }
 
     /**
