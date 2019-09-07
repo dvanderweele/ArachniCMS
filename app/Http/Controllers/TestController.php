1a258fdbfac4401a59c526e48e9f518beabc4a13
Dave VanderWeele <weele.me@gmail.com>
2019-09-07 12:27:51 -0500

    God, are you there?

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,12 +12,12 @@ class TestController extends Controller
     public function test($type){
         if ($type == 'backup'){
             GenerateBackup::dispatch();
-            return redirect()->route('/list-posts');
+            return redirect()->route('list-posts');
         }
         if ($type == 'sitemap'){
             SitemapGeneratorJob::dispatch();
-            return redirect()->route('/show-about');
+            return redirect()->route('show-about');
         }  
-        return redirect()->route('/create-contact');
+        return redirect()->route('create-contact');
     }
 }
