30439db45c8c916d119f2b563cc1ca0c18c6596f
Dave VanderWeele <weele.me@gmail.com>
2019-09-20 14:10:37 -0500

    removing ip dump testing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -87,7 +87,6 @@ class PostController extends Controller
      */
     public function show(Post $post, Request $request)
     {
-      dd($request->ip());
       $approved = Comment::where('post_id', $post->id)->where('approved', true)->latest()->paginate(5);
       $settings = Settings::firstOrFail();
       if($post->is_published)
