6c2ea4c7846dea755291724e58ecdbf0807267fb
Dave VanderWeele <weele.me@gmail.com>
2019-09-20 14:28:32 -0500

    further testing

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -207,7 +207,7 @@ class PostController extends Controller
           if (array_key_exists($key, $_SERVER) === true){
               foreach (explode(',', $_SERVER[$key]) as $ip){
                   $ip = trim($ip); // just to be safe
-                  if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE || FILTER_FLAG_NO_RES_RANGE) !== false){
+                  if (filter_var($ip, FILTER_VALIDATE_IP, FILTER_FLAG_NO_PRIV_RANGE | FILTER_FLAG_NO_RES_RANGE) !== false){
                       return $ip;
                   }
               }
